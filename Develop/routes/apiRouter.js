7ffa440b404da8105efece21db142ed96180f64a
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-12 22:09:33 -0400

    updates

--- Develop/routes/apiRouter.js
+++ Develop/routes/apiRouter.js
@@ -15,6 +15,41 @@ module.exports = (app) => {
 
     //GET REQUEST
     app.get('/api/notes', (req,res) => {
-
+        res.json(notes);
     });
-}
+
+    //POST REQUEST
+    app.post('/api/notes', (req,res) => {
+        const note = req.body;
+
+        //Now going to organize the notes by publish date:
+        //Loop to check the highest note id number:
+        let highest = 0;
+        for (let key in notes) {
+            let id = notes[key].id;
+            if (id > highest) {
+                highest = id;
+            }
+        }
+
+    //Add by one to the highest number to set to the new note:
+    note.id = highest + 1;
+    notes.push(note);
+
+    //Write new json element to the db.json file:
+    fs.writeFile("db/db.json", JSON.stringify(notes), function(err) {
+        
+        //Error checking
+        if (err) {
+            return console.log(err);
+        }
+
+        //Console log once complete.
+        console.log("Succesfully added to the db.json file a new JSON element.");
+        res.json(note);
+    
+    });//END FS.WRITEFILE
+
+    });//END POST REQUEST
+
+};//END MODULE
